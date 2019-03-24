d5c2ebb03d51308314e6b226714da84dff20bb22
Harry <harry@Harrys-MBP.fios-router.home>
2019-03-23 22:37:23 -0400

    fix device

--- enjoy.py
+++ enjoy.py
@@ -90,7 +90,7 @@ if "recurrent_policy" in model_params:
 actor_critic = Policy(env.observation_space.shape, env.action_space, device,
     base_kwargs={'recurrent': recurrent_policy})
 actor_critic.to(device)
-actor_critic.load_state_dict(torch.load(MODEL_SAVE_PATH, map_location='cpu'))
+actor_critic.load_state_dict(torch.load(MODEL_SAVE_PATH, map_location=device))
 
 
 # In[11]:
